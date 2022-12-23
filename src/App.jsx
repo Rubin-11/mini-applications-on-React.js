dc0fb458a61d6a080b11eb9dcf9618555cce7eca
Sergey Misharin <misharin_sergey-work@mail.ru>
2022-12-23 11:29:32 +0300

    counter

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './index.scss';
+
+function App() {
+
+const [count, setCount] = useState(0);
+
+const onClickPluse = () => {
+    setCount((count) => count + 1)
+}
+
+const onClickMinus = () => {
+    setCount((count) => count - 1)
+}
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик</h2>
+        <h1>{count}</h1>
+        <button className="minus" onClick={onClickMinus}>- Минус</button>
+        <button className="plus" onClick={onClickPluse}>Плюс +</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
